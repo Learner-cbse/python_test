5479afb13a4dab141455c883a2e5e67033e219b7
Learner-cbse <apu.mondal.cb@gmail.com>
2025-11-11 20:24:46 +0530

    first commit

---
+++ all_in_one.py
@@ -0,0 +1,301 @@
+# First py program hello world
+#print("Hello World!")
+
+# 2
+#print("My name is APU.")
+#print("My age is 23.")
+
+# 3
+#print("My name is APU.","My age is 23.")
+
+#4
+#print("My name is APU.","\nMy age is 23.")
+
+# 5
+#print(25)
+#print(20.53)
+#print(25+3)
+
+#======================= Variable =========================
+#6
+# String(str) variable
+#name = "apu"
+# Integer(int) variable
+#age = 23
+# Real Number(float) variable
+#price = 56.86
+#print(name, age, price)
+
+# 7
+#name = "Apu"
+#age = 23
+#print("My name is", name)
+#print("My age is", age)
+
+#======================= Data type() ===========================
+# 8
+#name = "Apu"
+#age = 23
+#price = 899.39
+#minor = False
+#income = None
+#print(type(name))
+#print(type(age))
+#print(type(price))
+#print(type(minor))
+#print(type(income))
+
+# 9
+#a = 84
+#b = 75
+#sum = a+b
+#print(sum)
+
+#========================= Comment =============================
+# 10
+# Single Line comment
+# good code
+#print("Good code")
+
+# 11
+# Multiple Line comment
+"""
+    good code
+    more line
+    more think
+"""
+#print("Good code","\nMore line","\nMore think")
+
+#====================== Operators ==============================
+# 12
+"""
+a = 63
+b = 7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+"""
+
+# 13
+"""
+num = 10
+num += 10
+print(num)
+
+num -= 10
+print(num)
+
+num *= 10
+print(num)
+
+num /= 10
+print(num)
+
+num //= 10
+print(num)
+
+num **= 10
+print(num)
+
+num %= 10
+print(num)
+"""
+
+# 14
+"""
+print(not True)
+print(not False)
+print(True or False)
+print(True and False)
+print(True and True)
+print(False or False)
+"""
+
+#=================== Type conversion(implicitly) ===================
+# 15
+"""
+a = 2
+b = 5.99
+sum = a + b
+print(sum)
+"""
+
+#=================== Type casting(explicitly) ======================
+# 16
+"""
+a = 20
+b = "30"
+sum = a + int(b)
+print(sum)
+print(type(sum))
+"""
+
+# 17
+"""
+a= 20
+b = "31.5"
+sum = a + float(b)
+print(sum)
+print(type(sum))
+"""
+
+# 18
+"""
+a = 123
+b = str(123)
+print(b)
+print(type(b))
+"""
+
+# 19
+"""
+_tuple = (1,2,3)
+_list = list(_tuple)
+print(_list)
+print(type(_list))
+"""
+
+# 20
+"""
+_list = [4,5,6]
+_tuple = tuple(_list)
+print(_tuple)
+print(type(_tuple))
+"""
+
+# 21
+"""
+li = [1,2,3,4]
+_set = set(li)
+print(_set)
+print(type(_set))
+"""
+
+# 22
+"""
+print(bool(0))
+print(bool())
+print(bool(""))
+print(bool(1))
+print(bool(978))
+print(bool(98.9))
+print(bool("A"))
+print(bool("hello"))
+"""
+
+#========================== Input ===========================
+# 23
+"""
+# String(str)
+name = input("Enter your name: ")
+print("My name is", name)
+
+# 24
+# Integer(int)
+age = int(input("Enter your age: "))
+print("My age is", age)
+
+# 25
+# Real Number(Float)
+price = float(input("Enter product price: "))
+print("Product price is", price)
+"""
+
+#===================== Quesation =============================
+# Write a program to input 2 numbers & print their sum.
+"""
+num1 = int(input("Enter 1st num: "))
+num2 = int(input("Enter 2nd num: "))
+sum = num1 + num2
+print(sum)
+"""
+
+# Write a program to input side of a square & print its area.
+"""
+side = int(input("Enter side: "))
+print("Area is", side * side)
+"""
+
+# Write a program to input 2 floating point numbers & print their average.
+"""
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+print("Average:", (a+b)/2)
+"""
+
+# Write a program to input 2 int numbers, a & b print True if a is greter then or equal to b. If not print False.
+"""
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+if a>b :
+    print(True)
+else :
+    print(False)
+"""
+
+#========================= Strings ==============================
+
+#str1 = "This is string"
+#str2 = 'ApuMondal'
+#str3 ="""this is a
+#string"""
+
+#print(str1+str2+str3)
+
+# single quotation inside the double quotation
+"""
+str1 = "This is boy's group"
+str2 = 'This is boy\'s group'
+str3 = "This is boy\'s group"
+print(str2)
+"""
+# Double quotation inside the triple quotation
+#str1 = """This is boy's group. Most powerfull group an "right hood" name of this group."""
+
+#print(str1)
+
+#======================= Concatination ============================
+"""
+str1 = 'Apu'
+str2 = 'Mondal'
+final  = str1 + str2
+
+print(final)
+"""
+
+#========================= Indexing =============================
+"""
+str = "APUMONDAL"
+
+print(str[0])   # Starting Index 
+print(str[3])
+#print(str[9])  # Show Error because indexing start in 0 number then
+                # 9 number of character not exist
+
+print(str[8])   # Ending Index
+"""
+
+# ========================= Slicing =============================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test1.py
@@ -0,0 +1,7 @@
+a = int(input("First Num : "))
+b = int(input("Second Num : "))
+
+if(a>b) :
+	print("True")
+else :
+	print("False")
